2f08d2b828d8a808652334bff79ef911790e8009
massimo marino <massimo.marino@ieee.org>
2025-02-18 19:37:15 +0100

    minor changes

--- src/cpplint.bash
+++ src/cpplint.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
-# 
-# File:   cpplint.bash
-# Author: massimo
 #
-# Created on May 11, 2017, 8:19:15 PM
+# File: cpplint.bash
 #
-cpplint --filter=-whitespace/braces  *.cpp *.h
+cpplint --filter=-whitespace/braces --recursive *
 #
--- src/oclint.bash
+++ src/oclint.bash
@@ -1,16 +1,12 @@
 #!/bin/bash
-# 
-# File:   oclint.bash
-# Author: massimo
 #
-# Created on May 10, 2017, 3:37:22 PM
+# File: oclint.bash
 #
-cp ../build/compile_commands.json .
+#find . -name '*.cpp' -print0 | xargs -0 clang-check -p ../build
 #
-oclint -enable-global-analysis \
-       -p ../build/ \
-       ./example/circular-buffer-example.cpp circularBuffer.cpp \
-       -- -std=gnu++17 -O3 -pthread -Wall -Weffc++ -Wextra -L/usr/lib/x86_64-linux-gnu -I/usr/include -lm
-#
-rm ./compile_commands.json
+clang-check -p ../build/ \
+            ./*.cpp \
+            ./example/*.cpp \
+            ./unitTests/*.cpp \
+            -- -std=c++20 -O0 -pthread -Wall -Wextra -pedantic -I. -lm
 #
